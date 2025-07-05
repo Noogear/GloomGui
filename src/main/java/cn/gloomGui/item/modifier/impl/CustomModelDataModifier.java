7ec040c654913468c8484cee4780bd8a367ed47c
Noogear <2364947161@qq.com>
2025-07-06 02:02:58 +0800

    Refactor CustomModelDataModifier meta editing logic
    Moved the call to editMeta inside the parseInteger presence check to ensure meta is only edited when a valid integer is parsed.

--- src/main/java/cn/gloomGui/item/modifier/impl/CustomModelDataModifier.java
+++ src/main/java/cn/gloomGui/item/modifier/impl/CustomModelDataModifier.java
@@ -33,8 +33,8 @@ public class CustomModelDataModifier implements ItemMetaModifier {
             this.customModelData = string;
             return true;
         } else {
-            original.editMeta(meta -> {
-                StringUtil.parseInteger(string).ifPresent(integer -> {
+            StringUtil.parseInteger(string).ifPresent(integer -> {
+                original.editMeta(meta -> {
                     meta.setCustomModelData(integer);
                 });
             });
